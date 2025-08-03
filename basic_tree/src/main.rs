6b8b43683f79b190151d01149cb01add59648fbb
Xavier Felix <129319353+XaviFelix@users.noreply.github.com>
2025-08-03 00:01:56 -0700

    Update main.rs

--- basic_tree/src/main.rs
+++ basic_tree/src/main.rs
@@ -1,3 +1,11 @@
+use tree_link::tree_ops::*;
+
 fn main() {
-    println!("Hello, world!");
+    let values = vec![1, 2, 3, 4, 5, 6, 7, 8];
+    let root = build_tree(&values);
+
+    let mut out = Vec::new();
+    inorder(&root, &mut out);
+    println!("in-order: {:?}", out);
+    println!("max depth: {:?}", max_depth(&root));
 }
